b7293e0b3da2d845ec12d9493ebbdab08c929ae5
woozxn <squib0612@gmail.com>
2022-03-21 20:26:08 +0900

    전체 조회 화면 개발

--- src/main/java/com/woozxn/board/domain/posts/PostsRepository.java
+++ src/main/java/com/woozxn/board/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.woozxn.board.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") //jpa로 가능하지만 가독성때문에 @Query 사용
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/woozxn/board/service/PostsService.java
+++ src/main/java/com/woozxn/board/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.woozxn.board.service;
 
 import com.woozxn.board.domain.posts.PostsRepository;
+import com.woozxn.board.web.dto.PostsListResponseDto;
 import com.woozxn.board.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true) //readOnly = true로 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
